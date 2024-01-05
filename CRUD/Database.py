941e03659e4496a4234ab522f3c76a9493e117fe
riosaputraduha <doehario@gmail.com>
2024-01-05 12:08:26 +0700

    init Database

--- CRUD/Database.py
+++ CRUD/Database.py
@@ -1,13 +1,19 @@
+from . import Operasi
+
+DB_NAME = "data.txt"
+TEMPLATE = {
+    'pk': "XXXXXX",
+    'date_add': "yyyy-mm-dd",
+    'judul': 255 * " ",
+    'penulis': 255 * " ",
+    'tahun': "yyyy"
+}
 
 def init_console():
     try:
-        with open("data.txt", "r") as file:
+        with open(DB_NAME, "r") as file:
             print("Database tersedia, init done!")
     except:
         print("Database tidak ditemukan, silakan membuat database baru")
-        with open("data.txt", "w", encoding="utf-8") as file:
-            penulis = input("Penulis: ")
-            judul = input("Judul: ")
-            tahun = input("Tahun: ")
-            data_str = f"{penulis}, {judul}, {tahun}"
-            file.write(data_str)
+        Operasi.create_first_data()
+            
